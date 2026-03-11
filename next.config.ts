8c174f5d87f7d53c3c32a0c4441d55f7529b1fe1
yappippipiyya <yappippipiyya@gmail.com>
2026-03-11 09:41:05 +0900

    pwa設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-export default nextConfig;
+export default withPWA({
+});
