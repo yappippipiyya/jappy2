50dc4bb2cdf496ebdc62c63b1d540a348e772bfb
yappippipiyya <yappippipiyya@gmail.com>
2026-03-11 09:53:39 +0900

    pwaの準備段階

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
       <head>
         <meta name="robots" content="noindex, nofollow" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className={`${notoSansJp.className} antialiased`}>
         <AlertProvider>
--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-const withPWA = withPWAInit({
-  dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  sw: "sw.js",
-  disable: process.env.NODE_ENV === "development",
-});
-
-export default withPWA(nextConfig);
+export default nextConfig;
