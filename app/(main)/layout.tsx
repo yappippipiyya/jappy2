61dcab72d92e8acb50d4ef1db859d6ea61bd3b52
yappippipiyya <yappippipiyya@gmail.com>
2026-03-13 15:58:27 +0900

    navbarをスクロールしたときに消えるようにした1

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/app/ui/footer"
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="min-h-screen pb-20 bg-zinc-50 dark:bg-zinc-950">
+    <main className="min-h-screen pt-22 pb-20 bg-zinc-50 dark:bg-zinc-950">
       <Navber />
       {children}
       <Footer />
